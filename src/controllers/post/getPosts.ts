df8e5e0a0619f367560e493e47ad94e3f0619608
= <pys99pys@gmail.com>
2019-12-04 08:44:06 +0900

    add status in response

--- src/controllers/post/getPosts.ts
+++ src/controllers/post/getPosts.ts
@@ -3,9 +3,10 @@ import 'reflect-metadata';
 
 import dbConn from 'utils/database';
 import { Post } from 'entity/Post';
+import { response } from 'utils/httpUtils';
 
 export default async (context: Context) => {
   const connection = await dbConn();
   const posts = await connection.manager.find(Post);
-  context.body = posts;
+  response(context, posts);
 };
--- src/utils/httpUtils.ts
+++ src/utils/httpUtils.ts
@@ -1,5 +1,10 @@
 import { Context } from 'koa';
 
+export const response = (context: Context, body: any) => {
+  context.status = 200;
+  context.body = body;
+};
+
 export const errorResponse = (
   context: Context,
   { status = 500, message = '처리중 오류가 발생했습니다.' }
